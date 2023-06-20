a8313971400f667281635e59c088ca01425ced99
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-06-21 00:31:40 +0200

    will do rest in the morning

--- Assets/Scripts/Class/Seat.cs
+++ Assets/Scripts/Class/Seat.cs
@@ -9,9 +9,23 @@ public class Seat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        studentImage = InitializeChild();
     }
+    Image InitializeChild()
+    {
+        foreach (Transform child in transform)
+        {
+            // Try to get the component from the current child GameObject
+            Image b = child.GetComponent<Image>();
 
+            // If the component is found, break the loop
+            if (b != null)
+                return b;
+           
+        }
+        Debug.Log("could not find any Images in the children of object " + gameObject);
+        return null;
+    }
     // Update is called once per frame
     void Update()
     {
