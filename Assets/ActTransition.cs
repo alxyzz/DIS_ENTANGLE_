d6b459bd0cc19347a845b1da84c35f2f85a04241
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-06-28 09:41:06 +0200

    transition stuff

---
+++ Assets/ActTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ActTransition : MonoBehaviour
+{
+    TextMeshProUGUI texty;
+
+
+
+    void Start()
+    {
+        texty.text = GameManager.Instance.GetTransitionText();
+        StartCoroutine(delayedTransition());
+    }
+
+
+    IEnumerator delayedTransition()
+    {
+        yield return new WaitForSecondsRealtime(GameManager.Instance.sceneTransitionDelay);
+
+        GameManager.Instance.Transition();
+
+
+    }
+}
